57afdf9eadc3f7eae4cdc31a9561f6174150561b
edwinbernadus <bernadus.edwin@gmail.com>
2023-11-03 12:49:29 +0700

    first commit

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,21 +3,51 @@
     windows_subsystem = "windows"
 )]
 
+use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[tauri::command]
-fn on_button_clicked() -> String {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_millis();
-    format!("on_button_clicked called from Rust! (timestamp: {since_the_epoch}ms)")
+#[derive(Debug, Deserialize, Clone, Serialize)]
+struct PlaceHolderItem {
+    userId: u32,
+    id: u32,
+    title: String,
 }
+
+#[tauri::command]
+fn fetch_data() -> Vec<PlaceHolderItem> {
+    println!("fetching data");
+    let client = Client::new();
+    let url = "https://jsonplaceholder.typicode.com/albums";
+
+    // hint_loading_webservice
+    let response = client.get(url).send();
+
+    match response {
+        Ok(mut response) => {
+            if response.status().is_success() {
+                let data: Vec<PlaceHolderItem> = response.json().unwrap();
+                return data;
+            } else {
+                // return empty array
+                let data: Vec<PlaceHolderItem> = [].to_vec();
+                return data;
+            }
+        }
+        Err(e) => {
+            // convert e to string
+            let data: Vec<PlaceHolderItem> = [].to_vec();
+            return data;
+        }
+    }
+}
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![on_button_clicked])
+        .invoke_handler(tauri::generate_handler![
+            fetch_data,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
